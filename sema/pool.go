f4e033c730a0e19ffd979288ad2b9bba815c0984
Tom Wiesing <tkw01536@gmail.com>
2023-04-30 02:34:45 +0200

    Add sema package

---
+++ sema/pool.go
@@ -0,0 +1,93 @@
+package sema
+
+import (
+	"sync"
+
+	"github.com/tkw1536/pkglib/lazy"
+	"golang.org/x/exp/slices"
+)
+
+// Pool holds a finite set of lazily created objects.
+type Pool[V any] struct {
+	// New creates a new item in the pool.
+	New func() V
+
+	// Discard is called before an item is (permanently) remove from the pool.
+	// Unlike a finalizer, Discard is guaranteed to be called.
+	Discard func(V)
+
+	// Limit is the maximum number of objects in the pool.
+	// Limit <= 0 means there is no limit on the number of objects in the pool.
+	// When Use requests an item, and the limit is already reached,
+	// Use will block until an item becomes available.
+	Limit int
+	s     lazy.Lazy[sync.Locker] // implements limit
+
+	l     sync.Mutex // lock protects items
+	items []V        // items holds the current items in the pool
+
+}
+
+// Use borrows an object from the pool, passes it to f, and then returns it to the pool.
+// Use blocks until f has returned.
+// If f returns an error (or f panics) the returned object is discarded, and a new object is created once needed.
+func (pool *Pool[V]) Use(f func(V) error) error {
+	// ensure that at most limit calls are active at the same time.
+	// this implements the limit on the items in the pool.
+	sema := pool.s.Get(func() sync.Locker { return NewSemaphore(pool.Limit) })
+	sema.Lock()
+	defer sema.Unlock()
+
+	// get or create a new item in the pool
+	var entry V
+	{
+		pool.l.Lock()
+
+		if len(pool.items) > 0 {
+			last := len(pool.items) - 1
+			entry = pool.items[last]
+			pool.items = slices.Clip(pool.items[:last])
+		} else {
+			entry = pool.New()
+		}
+
+		pool.l.Unlock()
+	}
+
+	// run the actual function
+	ok, err := func() (ok bool, err error) {
+		defer func() { recover() }()
+
+		ok = false
+		err = f(entry)
+		ok = err == nil
+		return
+	}()
+
+	// return the item to the pool (if everything went fine)
+	{
+		if ok {
+			pool.l.Lock()
+			pool.items = append(pool.items, entry)
+			pool.l.Unlock()
+		} else {
+			pool.Discard(entry)
+		}
+	}
+
+	// return the error
+	return err
+}
+
+// Close discards all objects currently in the pool.
+// Note that any active Use calls are not waited for.
+func (pool *Pool[V]) Close() {
+	pool.l.Lock()
+	defer pool.l.Unlock()
+
+	// call discard for all items
+	for _, item := range pool.items {
+		pool.Discard(item)
+	}
+	pool.items = nil
+}
---
+++ sema/pool_test.go
@@ -0,0 +1,122 @@
+package sema
+
+import (
+	"errors"
+	"fmt"
+	"sync"
+	"sync/atomic"
+
+	"golang.org/x/exp/slices"
+)
+
+type Thing uint64
+
+func (t *Thing) OK() error {
+	fmt.Printf("OK(%d)\n", *t)
+	return nil
+}
+
+var errTest = errors.New("test error")
+
+func (t *Thing) Fail() error {
+	fmt.Printf("Fail(%d)\n", *t)
+	return errTest
+}
+
+func (t *Thing) Close() {
+	fmt.Printf("Close(%d)\n", *t)
+}
+
+func ExamplePool() {
+	var counter uint64
+	p := Pool[*Thing]{
+		Limit: 1, // at most one item in the pool
+		New: func() *Thing {
+			// create a new thing (and print creating it)
+			id := atomic.AddUint64(&counter, 1)
+			fmt.Printf("New(%d)\n", id)
+			return ((*Thing)(&id))
+		},
+		Discard: (*Thing).Close,
+	}
+	defer p.Close()
+
+	// the first time an item from the pool is requested, it is created using New()
+	p.Use((*Thing).OK)
+
+	// calling it again, re-uses it
+	p.Use((*Thing).OK)
+
+	// failing causes it to be destroyed
+	p.Use((*Thing).Fail)
+
+	// and calling it again re-creates another one
+	p.Use((*Thing).OK)
+
+	// Output: New(1)
+	// OK(1)
+	// OK(1)
+	// Fail(1)
+	// Close(1)
+	// New(2)
+	// OK(2)
+	// Close(2)
+}
+
+func ExamplePool_Limit() {
+	var counter uint64
+
+	N := 10
+	M := 1000
+
+	var destroyedM sync.Mutex
+	var destroyed []uint64
+
+	p := Pool[uint64]{
+		Limit: 10, // at most one item in the pool
+		New: func() uint64 {
+			return atomic.AddUint64(&counter, 1)
+		},
+		Discard: func(u uint64) {
+			destroyedM.Lock()
+			defer destroyedM.Unlock()
+			destroyed = append(destroyed, u)
+		},
+	}
+
+	// fill the pool up with N items
+	var wg sync.WaitGroup
+	wg.Add(N)
+	done := make(chan struct{})
+
+	for i := 0; i < N; i++ {
+		go p.Use(func(u uint64) error {
+			wg.Done() // tell the outer loop an item has been created
+			<-done    // do not return the item to the pool until all have been created
+			return nil
+		})
+	}
+	wg.Wait()
+	close(done)
+
+	// use the items a bunch of times
+	wg.Add(M)
+	for i := 0; i < M; i++ {
+		go func() {
+			defer wg.Done()
+			p.Use(func(u uint64) error { return nil })
+		}()
+	}
+	wg.Wait()
+
+	// destroy all of them
+	// (this will record the items destroyed)
+	p.Close()
+
+	// we never had more than 10 items!
+	slices.Sort(destroyed)
+	fmt.Println(destroyed)
+
+	// Output: [1 2 3 4 5 6 7 8 9 10]
+
+}
