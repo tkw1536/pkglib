790a8577533b7a1f64faaa3796abe875b0f896a1
Tom Wiesing <tkw01536@gmail.com>
2025-04-02 21:33:12 +0200

    linting: Use only one tool

--- lint.sh
+++ lint.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 set -e
 
-echo "=> go vet"
-go vet ./...
-
-echo "=> staticcheck"
-go tool staticcheck ./...
-
 echo "=> golangci-lint"
 go tool golangci-lint run ./...
 
 echo "=> govulncheck"
 go tool govulncheck
-
-echo "=> gosec"
-go tool gosec ./...
