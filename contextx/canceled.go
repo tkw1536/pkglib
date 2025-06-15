f34c3c9080050da937c4e500d9970cb6e27449ea
Tom Wiesing <tkw01536@gmail.com>
2025-06-15 09:33:37 +0200

    contextx: Return same context

--- contextx/canceled.go
+++ contextx/canceled.go
@@ -5,6 +5,7 @@ package contextx
 import (
 	"context"
 	"errors"
+	"sync"
 )
 
 var errCanceled = errors.New("contextx.Canceled")
@@ -12,13 +13,17 @@ var errCanceled = errors.New("contextx.Canceled")
 // ErrCanceled is the cancel cause returned by Canceled.
 var ErrCanceled = errors.Join(context.Canceled, errCanceled)
 
-// Canceled returns a non-nil, empty Context.
-// It has no deadline, has no values, and is already canceled.
-// The cancel cause is ErrCanceled.
-//
-// Canceled may or may not return the same context for different invocations.
-func Canceled() context.Context {
+var canceled = sync.OnceValue(func() context.Context {
 	ctx, cancel := context.WithCancelCause(context.Background())
 	cancel(ErrCanceled)
 	return ctx
+})
+
+// Canceled returns a non-nil, empty Context.
+// It has no deadline, has no values, and is already canceled.
+// Calling [Cause] returns [ErrCanceled].
+//
+// Canceled may or may not returned the same context for different invocations.
+func Canceled() context.Context {
+	return canceled()
 }
--- contextx/canceled_test.go
+++ contextx/canceled_test.go
@@ -3,6 +3,8 @@ package contextx_test
 
 //spellchecker:words github pkglib contextx
 import (
+	"context"
+	"errors"
 	"fmt"
 
 	"github.com/tkw1536/pkglib/contextx"
@@ -18,5 +20,9 @@ func ExampleCanceled() {
 		fmt.Println("context was not canceled")
 	}
 
+	isContextXCancelled := errors.Is(context.Cause(ctx), contextx.ErrCanceled)
+	fmt.Printf("errors.Is(context.Cause(ctx), contextx.ErrCanceled) == %v\n", isContextXCancelled)
+
 	// Output: context was canceled
+	// errors.Is(context.Cause(ctx), contextx.ErrCanceled) == true
 }
