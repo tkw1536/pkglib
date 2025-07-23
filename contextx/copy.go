c700e9efafac6224c40fac6986af874ae8cff665
Tom Wiesing <tkw01536@gmail.com>
2025-07-23 05:42:59 +0200

    contextx/copy: Unexport CancelRead and CancelWrite
    This commit unexports the CancelRead and CancelWrite functions, as they
    were only used internally within the contextx package.

--- contextx/copy.go
+++ contextx/copy.go
@@ -12,9 +12,9 @@ import (
 )
 
 // Copy copies from src to dst, stopping once ctx is closed.
-// See io.Copy() for a description of the copy behavior.
+// See [io.Copy] for a description of the copy behavior.
 //
-// The operation is cancelled by closing the src and destination (if they support the Close() interface).
+// The operation is cancelled by closing the src and destination, if they support the [io.Closer] interface.
 // Furthermore appropriate read and write deadlines are set.
 // Either of these calls may not have any effect, depending on the underlying operation.
 func Copy(ctx context.Context, dst io.Writer, src io.Reader) (written int64, err error) {
@@ -26,15 +26,15 @@ func Copy(ctx context.Context, dst io.Writer, src io.Reader) (written int64, err
 		return io.Copy(dst, src)
 	}, func() {
 		// ignore any errors trying to cancel
-		_ = CancelRead(src)
-		_ = CancelWrite(dst)
+		_ = cancelRead(src)
+		_ = cancelWrite(dst)
 	})
 	return written, err
 }
 
-// CancelRead attempts to cancel any in-progress and future reads on the given reader.
+// cancelRead attempts to cancel any in-progress and future reads on the given reader.
 // In particular, this function sets the read deadline to the current time and closes the reader.
-func CancelRead(reader io.Reader) error {
+func cancelRead(reader io.Reader) error {
 	var errDeadline error
 	var errCloser error
 
@@ -49,9 +49,9 @@ func CancelRead(reader io.Reader) error {
 	return errorsx.Combine(errDeadline, errCloser)
 }
 
-// CancelWrite attempts to cancel any in-progress and future writes on the given writer.
+// cancelWrite attempts to cancel any in-progress and future writes on the given writer.
 // In particular, this function sets the write deadline to the current time and closes the writer.
-func CancelWrite(writer io.Writer) error {
+func cancelWrite(writer io.Writer) error {
 	var errDeadline error
 	var errCloser error
 
